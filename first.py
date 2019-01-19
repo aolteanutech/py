c304ee07b75ebe36cb492cb526d14f96adf1941d
unknown <aolteanutech@gmail.com>
2019-01-19 03:18:25 +0200

    added prompt

--- first.py
+++ first.py
@@ -1,2 +1,8 @@
-print "\nThis is the first python file to go to github"
+
+prompt = raw_input("\nIs this your first python file to go to github?")
+if prompt=="yes":
+    print "Ok, you need to add more"
+else:
+    print "Ok, how many have you added?"
+
 
